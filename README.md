fe26c9078353b129fb023d2cedb23b58b0ff5d89
Adrianus Indraprasta Dwicaksana <adrianprasta.bdg@gmail.com>
2023-05-17 22:26:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# capstone_project_tripease
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/capstone_project_tripease/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.capstone_project_tripease
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
